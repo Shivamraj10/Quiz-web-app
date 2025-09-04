f15f2d0d3122732fde4d2a372b36d73e024247cd
Shivam Raj <shivamrajpoddar01@gmail.com>
2025-09-05 03:23:25 +0530

    ALHAMDULLILAH showing question complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,28 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import StartQuiz from './assets/components/start';
 import './assets/css/app.css';
 import Quiz from './assets/components/quiz';
 
 function App() {
+  const [quiz, setQuiz] = useState([]);
+  const [question, setQuestion] = useState({});
+  const [questionIndex, setQuestionIndex] = useState(0);
+
+  // handle the quiz page
   const [showStart, setShowStart] = useState(true);
   const [showQuiz, setShowQuiz] = useState(false);
+
+  useEffect(() => {
+    fetch('/public/quiz.json')
+      .then((response) => response.json())
+      .then((data) => setQuiz(data));
+  }, []);
+
+  useEffect(() => {
+    if (quiz.length > questionIndex) {
+      setQuestion(quiz[questionIndex]);
+    }
+  }, [quiz, questionIndex]);
 
   const startQuiz = () => {
     setShowStart(false);
@@ -15,7 +32,7 @@ function App() {
   return (
     <>
       <StartQuiz startQuiz={startQuiz} showStart={showStart} />
-      <Quiz />
+      <Quiz quiz={quiz} showQuiz={showQuiz} question={question} />
     </>
   );
 }
--- src/assets/components/quiz.jsx
+++ src/assets/components/quiz.jsx
@@ -1,21 +1,36 @@
-const Quiz = () => {
+const Quiz = (props) => {
+  const { quiz, showQuiz, question } = props;
   return (
-    <section className="bg-dark text-white">
+    <section
+      className="bg-dark text-white"
+      style={{ display: `${showQuiz ? 'block' : 'none'}` }}
+    >
       <div className="container">
         <div className="row vh-100 align-items-center justify-content-center">
           <div className="col-lg-8">
             <div className="card p-4">
               <div className="d-flex justify-content-between gap-md-3">
-                <h5 className="mb-2 fs-normal lh-base">Question one</h5>
+                <h5 className="mb-2 fs-normal lh-base">{question?.question}</h5>
                 <h5
                   style={{
                     color: '#60d600',
                     width: '100px',
                     textAlign: 'right',
                   }}
-                ></h5>
+                >
+                  {quiz.indexOf(question) + 1} / {quiz.length}
+                </h5>
               </div>
-              <div>{/* some element */}</div>
+              <div>
+                {question?.options?.map((item, index) => (
+                  <button
+                    key={index}
+                    className={`option w-100 text-start btn text-white py-2 px-3 mt-3 rounded btn-dark`}
+                  >
+                    {item}
+                  </button>
+                ))}
+              </div>
 
               {/* some element */}
             </div>
