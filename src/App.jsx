f0a6c13d18ea173e40fdc6fb8aee6b784030b606
Shivam Raj <shivamrajpoddar01@gmail.com>
2025-09-05 03:23:25 +0530

    react project quiz running

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,21 @@
+import { useState } from 'react';
+import StartQuiz from './assets/components/start';
+import './assets/css/app.css';
+
 function App() {
-  return <>react quiz app</>;
+  const [showStart, setShowStart] = useState(true);
+  const [showQuiz, setShowQuiz] = useState(false);
+
+  const startQuiz = () => {
+    setShowStart(false);
+    setShowQuiz(true);
+  };
+
+  return (
+    <>
+      <StartQuiz startQuiz={startQuiz} showStart={showStart} />
+    </>
+  );
 }
 
 export default App;
